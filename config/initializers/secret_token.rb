52f05e8b639caa46a95a6fd9bb595b3a6a791e74
Xavi Aracil <xavi@xadsolutions.com>
2012-06-08 12:35:15 +0200

    rails new webapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basketlleure::Application.config.secret_token = '358805b27cb6705e2297166e1b6e186129b747ccc9eed1a23272b2535b2f900d86906c089f09aa5caa71741f2a50029fdb9a070acae6326f65aadff0259cfe19'
